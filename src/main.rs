e1dc64ddc3a08533063c9cc55204724aeacc16ef
David Cristofaro <david@dtcristo.com>
2019-07-16 16:16:24 +1000

    Make window size variable

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-use minifb::{Key, MouseButton, MouseMode, Window, WindowOptions};
+use minifb::{Key, MouseButton, MouseMode, Scale, Window, WindowOptions};
 use palette::{encoding::pixel::Pixel, Gradient, Hsv, LinSrgb};
 use rayon::prelude::*;
 use std::time::SystemTime;
 
-const WIDTH: usize = 1024;
-const HEIGHT: usize = 768;
 const GRADIENT_SIZE: usize = 100;
 const MAX_ITERATIONS: usize = 1000;
 
 fn main() {
+    let window_size = (1024, 768);
     let mut centre = (-0.666_f64, 0.0_f64);
     let mut zoom = 0;
     let mut mouse_left_was_down = false;
     let mut mouse_right_was_down = false;
-    let mut buffer = vec![0_u32; WIDTH * HEIGHT];
-    let mut stale_buffer = true;
-    let mut window = Window::new("mandelbrot", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
+    let mut do_render = true;
+    let mut window = Window::new(
+        "mandelbrot",
+        window_size.0,
+        window_size.1,
+        WindowOptions {
+            resize: true,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap();
 
     let palette: Vec<u32> = Gradient::new(vec![
         Hsv::from(LinSrgb::new(0.0, 1.0, 1.0)),
@@ -32,50 +39,63 @@ fn main() {
     .collect();
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
+        let old_window_size = window_size;
+        // window_size = window.get_size();
         let mouse_left_down = window.get_mouse_down(MouseButton::Left);
         let mouse_right_down = window.get_mouse_down(MouseButton::Right);
-        if mouse_left_was_down && !mouse_left_down {
+        if window_size != old_window_size {
+            println!("window resized {:?}", window_size);
+            do_render = true;
+        } else if mouse_left_was_down && !mouse_left_down {
             if let Some(mouse_pos) = window.get_unscaled_mouse_pos(MouseMode::Clamp) {
-                centre = mouse_coords(centre, zoom, mouse_pos);
+                centre = mouse_coords(window_size, centre, zoom, mouse_pos);
                 zoom += 1;
-                stale_buffer = true;
+                do_render = true;
             }
         } else if mouse_right_was_down && !mouse_right_down && zoom > 0 {
             zoom -= 1;
             if zoom == 0 {
                 centre = (-0.666, 0.0);
             }
-            stale_buffer = true;
+            do_render = true;
         }
         mouse_left_was_down = mouse_left_down;
         mouse_right_was_down = mouse_right_down;
 
-        if stale_buffer {
+        if do_render {
             let start = SystemTime::now();
-            buffer = render(centre, zoom, &palette);
+            let buffer = render(window_size, centre, zoom, &palette);
             let end = SystemTime::now();
             let duration = end.duration_since(start).unwrap();
             println!("done in {:?}", duration);
-            stale_buffer = false;
+            window.update_with_buffer(&buffer).unwrap();
+            do_render = false;
+        } else {
+            window.update();
         }
-        window.update_with_buffer(&buffer).unwrap();
     }
 }
 
-fn render(centre: (f64, f64), zoom: usize, palette: &[u32]) -> Vec<u32> {
+fn render(
+    window_size: (usize, usize),
+    centre: (f64, f64),
+    zoom: usize,
+    palette: &[u32],
+) -> Vec<u32> {
     println!(
         "centre {:?}, zoom {}, {} iterations... ",
         centre, zoom, MAX_ITERATIONS
     );
+
     let (x_min, x_max, y_min, y_max) = frame_bounds(centre, zoom);
-    (0..HEIGHT)
+    (0..window_size.1)
         .into_par_iter()
         .map(|row| {
-            let y = (row as f64 / HEIGHT as f64) * (y_min - y_max) + y_max;
-            (0..WIDTH)
+            let y = (row as f64 / window_size.1 as f64) * (y_min - y_max) + y_max;
+            (0..window_size.0)
                 .into_par_iter()
                 .map(|column| {
-                    let x = (column as f64 / WIDTH as f64) * (x_max - x_min) + x_min;
+                    let x = (column as f64 / window_size.0 as f64) * (x_max - x_min) + x_min;
                     let iterations = iterate(x, y);
                     if iterations == MAX_ITERATIONS {
                         0
@@ -114,9 +134,14 @@ fn frame_bounds(centre: (f64, f64), zoom: usize) -> (f64, f64, f64, f64) {
     (x_min, x_max, y_min, y_max)
 }
 
-fn mouse_coords(centre: (f64, f64), zoom: usize, mouse_pos: (f32, f32)) -> (f64, f64) {
+fn mouse_coords(
+    window_size: (usize, usize),
+    centre: (f64, f64),
+    zoom: usize,
+    mouse_pos: (f32, f32),
+) -> (f64, f64) {
     let (x_min, x_max, y_min, y_max) = frame_bounds(centre, zoom);
-    let x = (f64::from(mouse_pos.0) / WIDTH as f64) * (x_max - x_min) + x_min;
-    let y = (f64::from(mouse_pos.1) / HEIGHT as f64) * (y_min - y_max) + y_max;
+    let x = (f64::from(mouse_pos.0) / window_size.0 as f64) * (x_max - x_min) + x_min;
+    let y = (f64::from(mouse_pos.1) / window_size.1 as f64) * (y_min - y_max) + y_max;
     (x, y)
 }
