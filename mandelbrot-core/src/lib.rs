a567efba7672064590ad29788315aaab06ca9ee3
David Cristofaro <david@dtcristo.com>
2019-07-17 11:54:04 +1000

    Refactor into workspace

---
+++ mandelbrot-core/src/lib.rs
@@ -0,0 +1,79 @@
+use palette::{encoding::pixel::Pixel, Gradient, Hsv, LinSrgb};
+use wasm_bindgen::prelude::*;
+
+const GRADIENT_SIZE: usize = 100;
+
+#[wasm_bindgen]
+pub fn render(
+    frame_width: usize,
+    frame_height: usize,
+    centre_x: f64,
+    centre_y: f64,
+    zoom: usize,
+    max_iterations: usize,
+) -> Vec<u32> {
+    let centre = (centre_x, centre_y);
+    println!(
+        "centre {:?}, zoom {}, {} iterations... ",
+        centre, zoom, max_iterations
+    );
+
+    // TODO: Pre-generate palette
+    let palette: Vec<u32> = Gradient::new(vec![
+        Hsv::from(LinSrgb::new(0.0, 1.0, 1.0)),
+        Hsv::from(LinSrgb::new(1.0, 0.0, 0.0)),
+        Hsv::from(LinSrgb::new(0.0, 1.0, 1.0)),
+    ])
+    .take(GRADIENT_SIZE)
+    .map(|color| {
+        let pixel: [u8; 3] = LinSrgb::from(color).into_format().into_raw();
+        (u32::from(pixel[0]) << 16) | (u32::from(pixel[1]) << 8) | (u32::from(pixel[2]))
+    })
+    .cycle()
+    .take(max_iterations)
+    .collect();
+
+    let (x_min, x_max, y_min, y_max) = frame_bounds(centre_x, centre_y, zoom);
+    (0..frame_height)
+        .map(|row| {
+            let y = (row as f64 / frame_height as f64) * (y_min - y_max) + y_max;
+            (0..frame_width)
+                .map(|column| {
+                    let x = (column as f64 / frame_width as f64) * (x_max - x_min) + x_min;
+                    let i = iterate(x, y, max_iterations);
+                    if i == max_iterations {
+                        0
+                    } else {
+                        palette[i]
+                    }
+                })
+                .collect::<Vec<u32>>()
+        })
+        .flatten()
+        .collect()
+}
+
+fn iterate(c_x: f64, c_y: f64, max_iterations: usize) -> usize {
+    let mut z_x = 0.0;
+    let mut z_y = 0.0;
+    let mut i = 0;
+    while i < max_iterations && z_x * z_x + z_y * z_y <= 4.0 {
+        let z_x_new = z_x * z_x - z_y * z_y + c_x;
+        let z_y_new = 2.0 * z_x * z_y + c_y;
+        z_x = z_x_new;
+        z_y = z_y_new;
+        i += 1;
+    }
+    i
+}
+
+pub fn frame_bounds(centre_x: f64, centre_y: f64, zoom: usize) -> (f64, f64, f64, f64) {
+    let zoom_scale = 2.0_f64.powf(zoom as f64);
+    let delta_x = 1.666 * (1.0 / zoom_scale);
+    let delta_y = 1.25 * (1.0 / zoom_scale);
+    let x_min = centre_x - delta_x;
+    let x_max = centre_x + delta_x;
+    let y_min = centre_y - delta_y;
+    let y_max = centre_y + delta_y;
+    (x_min, x_max, y_min, y_max)
+}
---
+++ mandelbrot-desktop/src/main.rs
@@ -0,0 +1,74 @@
+use minifb::{Key, MouseButton, MouseMode, Window, WindowOptions};
+use std::time::SystemTime;
+
+use mandelbrot_core::{frame_bounds, render};
+
+const MAX_ITERATIONS: usize = 1000;
+
+fn main() {
+    let window_size = (1024, 768);
+    let mut centre = (-0.666_f64, 0.0_f64);
+    let mut zoom = 0;
+    let mut mouse_left_was_down = false;
+    let mut mouse_right_was_down = false;
+    let mut do_render = true;
+    let mut window = Window::new(
+        "mandelbrot",
+        window_size.0,
+        window_size.1,
+        WindowOptions {
+            resize: false,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap();
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        let mouse_left_down = window.get_mouse_down(MouseButton::Left);
+        let mouse_right_down = window.get_mouse_down(MouseButton::Right);
+        // let old_window_size = window_size;
+        // window_size = window.get_size();
+        // if window_size != old_window_size {
+        //     println!("window resized {:?}", window_size);
+        //     do_render = true;
+        /*} else */ if mouse_left_was_down && !mouse_left_down {
+            if let Some(mouse_pos) = window.get_unscaled_mouse_pos(MouseMode::Clamp) {
+                centre = mouse_coords(window_size, centre, zoom, mouse_pos);
+                zoom += 1;
+                do_render = true;
+            }
+        } else if mouse_right_was_down && !mouse_right_down && zoom > 0 {
+            zoom -= 1;
+            if zoom == 0 {
+                centre = (-0.666, 0.0);
+            }
+            do_render = true;
+        }
+        mouse_left_was_down = mouse_left_down;
+        mouse_right_was_down = mouse_right_down;
+
+        if do_render {
+            let start = SystemTime::now();
+            let buffer = render(window_size.0, window_size.1, centre.0, centre.1, zoom, MAX_ITERATIONS);
+            let end = SystemTime::now();
+            let duration = end.duration_since(start).unwrap();
+            println!("done in {:?}", duration);
+            window.update_with_buffer(&buffer).unwrap();
+            do_render = false;
+        } else {
+            window.update();
+        }
+    }
+}
+
+fn mouse_coords(
+    window_size: (usize, usize),
+    centre: (f64, f64),
+    zoom: usize,
+    mouse_pos: (f32, f32),
+) -> (f64, f64) {
+    let (x_min, x_max, y_min, y_max) = frame_bounds(centre.0, centre.1, zoom);
+    let x = (f64::from(mouse_pos.0) / window_size.0 as f64) * (x_max - x_min) + x_min;
+    let y = (f64::from(mouse_pos.1) / window_size.1 as f64) * (y_min - y_max) + y_max;
+    (x, y)
+}
