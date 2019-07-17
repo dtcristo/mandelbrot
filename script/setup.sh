efa24ddf96c3fb2350ed80d019ad7c0c7d8ba6be
David Cristofaro <david@dtcristo.com>
2019-07-17 17:03:26 +1000

    Fix up PATH

--- script/setup.sh
+++ script/setup.sh
@@ -4,5 +4,5 @@ rustup target add wasm32-unknown-unknown
 cargo install wasm-pack --force
 cd mandelbrot-web/ && npm install && cd ../
 npm set prefix=/home/circleci/npm
-echo 'export PATH=$HOME/circleci/npm/bin:$PATH' >> /home/circleci/.bashrc
+echo 'export PATH=/home/circleci/npm/bin:$PATH' >> /home/circleci/.bashrc
 npm install -g firebase-tools
