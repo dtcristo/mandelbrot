99cd62ee352a0b3a27ccdd329f3423422d265cb0
David Cristofaro <david@dtcristo.com>
2019-07-17 17:28:51 +1000

    Fix setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -3,7 +3,7 @@ set -e
 rustup target add wasm32-unknown-unknown
 cargo install wasm-pack --force
 cd mandelbrot-web/ && npm install && cd ../
-if [ "$CIRCLECI" == "true" ]; then
+if [ "$CIRCLECI" = "true" ]; then
   npm set prefix=/home/circleci/npm
   echo 'PATH=/home/circleci/npm/bin:$PATH' >> /home/circleci/.profile
   source /home/circleci/.profile
