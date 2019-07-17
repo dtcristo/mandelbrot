962a19f71041cbf1fb1805ef1988ba1b4dce071c
David Cristofaro <david@dtcristo.com>
2019-07-17 17:34:07 +1000

    Update syntax for source

--- script/setup.sh
+++ script/setup.sh
@@ -6,6 +6,6 @@ cd mandelbrot-web/ && npm install && cd ../
 if [ "$CIRCLECI" = "true" ]; then
   npm set prefix=/home/circleci/npm
   echo 'PATH=/home/circleci/npm/bin:$PATH' >> /home/circleci/.profile
-  source /home/circleci/.profile
+  . /home/circleci/.profile
 fi
 npm install -g firebase-tools
