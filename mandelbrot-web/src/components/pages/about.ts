be3dfd1a597853c654784f7d66ad9e0b5e14f2eb
David Cristofaro <david@dtcristo.com>
2019-07-29 18:51:04 +1000

    Add dummy content

--- mandelbrot-web/src/components/pages/about.ts
+++ mandelbrot-web/src/components/pages/about.ts
@@ -6,7 +6,41 @@ import BaseComponent from "../base_component";
 export default class About extends BaseComponent {
   render() {
     return html`
-      <h2>About</h2>
+      <section class="section">
+        <div class="container">
+          <div class="content">
+            <h1>About</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              accumsan, metus ultrices eleifend gravida, nulla nunc varius
+              lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui.
+              Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.
+            </p>
+            <h2>Second level</h2>
+            <p>
+              Curabitur accumsan turpis pharetra
+              <strong>augue tincidunt</strong> blandit. Quisque condimentum
+              maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna
+              vel cursus venenatis. Suspendisse potenti. Etiam mattis sem
+              rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et
+              neque nisl.
+            </p>
+            <ul>
+              <li>
+                In fermentum leo eu lectus mollis, quis dictum mi aliquet.
+              </li>
+              <li>
+                Morbi eu nulla lobortis, lobortis est in, fringilla felis.
+              </li>
+              <li>
+                Aliquam nec felis in sapien venenatis viverra fermentum nec
+                lectus.
+              </li>
+              <li>Ut non enim metus.</li>
+            </ul>
+          </div>
+        </div>
+      </section>
     `;
   }
 }
--- mandelbrot-web/src/components/pages/gallery.ts
+++ mandelbrot-web/src/components/pages/gallery.ts
@@ -6,7 +6,19 @@ import BaseComponent from "../base_component";
 export default class Gallery extends BaseComponent {
   render() {
     return html`
-      <h2>Gallery</h2>
+      <section class="section">
+        <div class="container">
+          <div class="content">
+            <h1>Gallery</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              accumsan, metus ultrices eleifend gravida, nulla nunc varius
+              lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui.
+              Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.
+            </p>
+          </div>
+        </div>
+      </section>
     `;
   }
 }
