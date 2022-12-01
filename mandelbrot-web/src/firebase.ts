66de3aafa6d10ef179a0905e40c823a7a440a15b
David Cristofaro <david@dtcristo.com>
2022-12-01 22:59:34 +1100

    Properly remove firebase

--- mandelbrot-web/src/firebase.ts
+++
@@ -1,5 +0,0 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-
-export const app = firebase.initializeApp({ projectId: "mandelbrot-9b386" });
-export const firestore = firebase.firestore();
